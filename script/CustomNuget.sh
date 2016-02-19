a70addfb71928a596bf8e0178f05e93ab83cb490
SocialShapr <social@shapr.net>
2016-02-19 16:02:33 +0100

    Update CustomNuget.sh

--- script/CustomNuget.sh
+++ script/CustomNuget.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nuget sources add -Name ${NAME_FOR_SOURCE} -Source ${SOURCE_URL} -UserName ${NUGET_USERNAME} -Password ${NUGET_PASSWORD}
+nuget sources add -Name ${NAME_FOR_SOURCE} -Source ${SOURCE_URL}
